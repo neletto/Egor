bb534d9e18bc9578f1f18414b569f13d5ee5ad4b
Egor <segor8185@gmail.com>
2024-02-08 10:17:34 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log('arr '+ " - object");
-console.log(4 + +"5");
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let incr = 10,
-    decr = 10;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;    
 
-// ++incr;
-// --decr;
-
-console.log(++incr);
-console.log(--decr);
-
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !=="6");
-
-
-
-const isChecked = true;
-      isClose = false;
-console.log(isChecked || !isClose); 
+console.log(personalMovieDB);
